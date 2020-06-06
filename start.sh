1e1f0c20aa1f9e96a585ed4ed8edbfa28bb35265
Ankush Kale <ankushkale1@gmail.com>
2020-06-06 20:53:33 +0530

    changed magic url js, added try catch so if document have any malformed url it will not throw error, previously it was throwing error so image was not getting pasted via ctrl + V

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker-compose -f docker-compose-local.yml up -d mysqldb
+
 java -Xverify:none -XX:+UseStringDeduplication -Xms32m \
 -Xmx2048m \
 -XX:MaxHeapFreeRatio=15 \
@@ -8,4 +10,10 @@ java -Xverify:none -XX:+UseStringDeduplication -Xms32m \
 -XX:+UseShenandoahGC \
 -XX:ShenandoahUncommitDelay=10000 \
 -XX:ShenandoahGuaranteedGCInterval=60000 \
+-XX:MetaspaceSize=16M \
+-XX:MinMetaspaceFreeRatio=10 \
+-XX:MaxMetaspaceFreeRatio=15 \
+-XX:MinMetaspaceExpansion=1M \
+-XX:MaxMetaspaceExpansion=5M \
+-Xss500k \
 -XX:MaxGCPauseMillis=1000 -jar target/NotesApp-0.0.1.jar &
