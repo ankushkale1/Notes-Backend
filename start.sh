898e06d540547423bb190b489d253ae6b363b4f6
Ak <ankushk@rediff-inc.com>
2026-03-04 01:06:51 +0530

    start script updated

--- start.sh
+++ start.sh
@@ -2,20 +2,25 @@
 
 docker-compose -f docker-compose-local.yml up -d mysqldb
 
-java -Xverify:none -XX:+UseStringDeduplication -Xms32m \
-  -Xmx2048m \
-  -XX:MaxHeapFreeRatio=15 \
-  -XX:MinHeapFreeRatio=5 \
+# Kill any existing instance of the app before starting
+pkill -f NotesApp-0.0.1.jar || true
+
+java -Xverify:none \
+  -XX:+UseStringDeduplication \
+  -Xms64m \
+  -Xmx768m \
+  -XX:MaxHeapFreeRatio=20 \
+  -XX:MinHeapFreeRatio=10 \
   -XX:+UnlockExperimentalVMOptions \
   -XX:+UseShenandoahGC \
-  -XX:ShenandoahUncommitDelay=10000 \
-  -XX:ShenandoahGuaranteedGCInterval=60000 \
-  -XX:CompressedClassSpaceSize=70M \
-  -XX:MetaspaceSize=16M \
+  -XX:ShenandoahUncommitDelay=5000 \
+  -XX:ShenandoahGuaranteedGCInterval=30000 \
+  -XX:CompressedClassSpaceSize=64M \
+  -XX:MetaspaceSize=32M \
   -XX:MinMetaspaceFreeRatio=10 \
-  -XX:MaxMetaspaceFreeRatio=15 \
-  -XX:MinMetaspaceExpansion=1M \
-  -XX:MaxMetaspaceExpansion=5M \
-  -Xss500k \
-  -XX:NativeMemoryTracking=detail \
-  -XX:MaxGCPauseMillis=1000 -jar target/NotesApp-0.0.1.jar &
+  -XX:MaxMetaspaceFreeRatio=20 \
+  -Xss256k \
+  -XX:MaxGCPauseMillis=500 \
+  -jar target/NotesApp-0.0.1.jar > app.log 2>&1 &
+
+echo "NoteBook App starting on port 8999..."
