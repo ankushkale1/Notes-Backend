f1c80bd5878a04907efe56199832853879e3d352
Ankush Kale <ankushkale1@gmail.com>
2026-08-10 18:37:49 +0530

    dump script updated

--- dump.sh
+++ dump.sh
@@ -3,6 +3,18 @@ cd dumps
 dt=$(date +%Y_%m_%d_%s)
 mkdir $dt
 cd $dt
-mysqldump --column-statistics=0 -h127.0.0.1 -uroot -proot@123 notesdb >$dt.sql
+
+mysqldump -h127.0.0.1 -uroot -proot@123 \
+  --single-transaction \
+  --quick \
+  --max-allowed-packet=1G \
+  --net-buffer-length=16384 \
+  --routines \
+  --triggers \
+  --events \
+  --default-character-set=utf8mb4 \
+  --add-drop-table \
+  --complete-insert \
+  notesdb > $dt.sql
 
 gzip $dt.sql
